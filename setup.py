898cccc443638ea639b267b370b2e7cf96e29949
VinitKumar536 <vinitjangra536@gmail.com>
2026-07-08 16:32:09 +0530

    few changes , base don

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+setup(
+    name="src",
+    packages=find_packages(),
+    version="0.0.1",
+    author="Vinit Jangra ",
+    author_email="vinitjangra536@gmail.com",
+    install_requires=[]
+)
